a1c976d3f5fb98293311788998cd10849ebbcc91
Jayce <jstephe3@xula.edu>
2025-10-05 20:21:27 -0400

    Add test for Horizontal Histogram

--- src/test/java/teamred/OddTest.java
+++ src/test/java/teamred/OddTest.java
@@ -22,6 +22,24 @@ public class OddTest {
 
     }
 
+    @DriverTest
+    public void testHorizontalHistogram(){
+         // Arrange
+        List<InsuranceRecord> records = Arrays.asList(
+            new InsuranceRecord(30, 22.0, 0, 2000.0, "northwest", "no","male"),
+            new InsuranceRecord(40, 28.0, 1, 3000.0, "southeast", "yes", "female"),
+            new InsuranceRecord(50, 35.0, 2, 4000.0, "southwest", "no", "male")
+        );
+
+        // Act
+        String histogram = InsuranceProblems.horizontalAgeHistogram(records);
+
+        // Assert
+        assertTrue(histogram.contains("20: **"));
+        assertTrue(histogram.contains("30: *"));
+    }
+
+
     
 
     
