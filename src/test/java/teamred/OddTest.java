2249b2310411e220491eca6d96d3f9fe3707ece0
Jayce <jstephe3@xula.edu>
2025-10-05 20:16:45 -0400

    Add problem 1 test

---
+++ src/test/java/teamred/OddTest.java
@@ -0,0 +1,28 @@
+package teamred;
+
+
+import java.util.*;
+
+public class OddTest {
+    
+    @DriverTest
+    void dataSetRecords(){
+        // Arrange
+        List<InsuranceRecord> records = Arrays.asList(
+            new InsuranceRecord(30, 22.0, 0, 2000.0, "northwest", "no","male"),
+            new InsuranceRecord(40, 28.0, 1, 3000.0, "southeast", "yes", "female"),
+            new InsuranceRecord(50, 35.0, 2, 4000.0, "southwest", "no", "male")
+        );
+        // Act
+        List<InsuranceRecord> result = InsuranceProblems.getFirstNRecords(records, 2);
+        // Assert
+        assertEquals(2, result.size());
+        assertEquals(20, result.get(0).age);
+        assertEquals(30, result.get(1).age);
+
+    }
+
+    
+
+    
+}
